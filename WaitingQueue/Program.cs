369f99f4c673353182ed2ba332c1f091fbcd3abc
Harold <pxduharold@gmail.com>
2020-08-09 18:32:37 +0800

    first commit

---
+++ WaitingQueue/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace WaitingQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var wq = new WaitingQueue<int>(10);
+
+            Console.WriteLine($"wq is empty: {wq.IsEmpty()}");
+            Console.WriteLine($"can dequeue: {wq.CanDequeue()}");
+
+            for (int i = 0; i < 10; i++)
+            {
+                wq.Enqueue(i);
+            }
+
+            Console.WriteLine($"wq is full: {wq.IsFull()}");
+
+            Thread thread1 = new Thread(()=> {
+                Thread.Sleep(1000);
+                var i = wq.Dequeue();
+                Console.WriteLine($"thread1 deque: {i}");
+            });
+
+            var stopwatch = new Stopwatch();
+
+            thread1.Start();
+            
+            stopwatch.Start();
+            Console.WriteLine($"can enqueue: {wq.CanEnqueue()}");
+            wq.EnqueueWaitAvailable(11);
+            Console.WriteLine($"main thread enqueue: 11");
+            Console.WriteLine($"time elapsed: {stopwatch.Elapsed}");
+            Console.WriteLine($"wq is full: {wq.IsFull()}");
+            for (int i = 9; i > 0; i--)
+                wq.Dequeue();
+            Console.WriteLine($"last item: {wq.Dequeue()}");
+            Console.WriteLine($"wq is empty: {wq.IsEmpty()}");
+
+            Thread thread2 = new Thread(() =>
+            {
+                Thread.Sleep(1000);
+                wq.Enqueue(12);
+                Console.WriteLine($"thread2 enquque: 12");
+            });
+
+            stopwatch = new Stopwatch();
+
+            thread2.Start();
+
+            stopwatch.Start();
+            Console.WriteLine($"can dequeue: {wq.CanDequeue()}");
+            var x = wq.DequeueWaitAvailable();
+            Console.WriteLine($"main thread dequeue: {x}");
+            Console.WriteLine($"time elapsed: {stopwatch.Elapsed}");
+            Console.WriteLine($"wq is empty: {wq.IsEmpty()}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ WaitingQueue/WaitingQueue.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace WaitingQueue
+{
+    public class WaitingQueue<T> : IDisposable
+    {
+        private readonly Queue<T> items;
+        private readonly int cap;
+        private static readonly object locker = new object();
+        private static readonly AutoResetEvent newItemEvt = new AutoResetEvent(false);
+        private static readonly ManualResetEvent queueNotFullEvt = new ManualResetEvent(true);
+
+        public WaitingQueue(int capacity)
+        {
+            cap = capacity;
+            items = new Queue<T>(capacity);
+        }
+
+        public void Dispose()
+        {
+            items.Clear();
+        }
+
+        public void Clear()
+        {
+            items.Clear();
+        }
+
+        public bool IsEmpty()
+        {
+            lock(locker)
+            {
+                return items.Count == 0;
+            }
+        }
+
+        public bool IsFull()
+        {
+            lock(locker)
+            {
+                return items.Count == cap;
+            }
+        }
+
+        // Flush old item if full
+        public void Enqueue(T t)
+        {
+            lock(locker)
+            {
+                items.Enqueue(t);
+                newItemEvt.Set();
+                // if full, reset flag
+                if (items.Count == cap)
+                    queueNotFullEvt.Reset();
+            }
+        }
+
+        // Throw exception if empty
+        public T Dequeue()
+        {
+            lock(locker)
+            {
+                queueNotFullEvt.Set();
+                return items.Dequeue();
+            }
+        }
+
+        // dequeue until item inside
+        public T DequeueWaitAvailable()
+        {
+        OUT:
+            try
+            {
+                return Dequeue();
+            }
+            catch (InvalidOperationException)
+            {
+                while (newItemEvt.WaitOne())
+                {
+                    goto OUT;
+                }
+                throw;
+            }
+        }
+
+        // enqueue without flush
+        public void EnqueueWaitAvailable(T t)
+        {
+        IN:
+            // check flag, if not full(set), enqueue
+            if (queueNotFullEvt.WaitOne(0))
+            {
+                Enqueue(t);
+                return;
+            }
+            // if full(unset), wait
+            queueNotFullEvt.WaitOne();
+            goto IN;
+        }
+
+        public bool CanDequeue()
+        {
+            return newItemEvt.WaitOne(0);
+        }
+
+        public bool CanEnqueue()
+        {
+            return queueNotFullEvt.WaitOne(0);
+        }
+    }
+}
